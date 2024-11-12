d2ae7d0dfc977eb635726095c5e6a8f7af12809c
omerKamran2 <omer.kamran100@gmail.com>
2024-11-13 02:12:41 +0500

    added header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+
+import Header from "@/components/header";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,6 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+    
+      <Header/>
+
+
         {children}
       </body>
     </html>
