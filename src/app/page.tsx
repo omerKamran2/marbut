ea1e9fa2ccdeab4bde965d6f6bb227d8e68d0063
omerKamran2 <omer.kamran100@gmail.com>
2024-11-13 03:09:38 +0500

    add success stories placeholder to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
 import Header from "@/components/header";
 import About from "@/components/about";
+import SuccessStories from "@/components/succes_stories";
 
 export default function Home() {
   return (
     <main>
       <Header/>
       <About/>
+      <SuccessStories/>
     </main>
   );
 }
