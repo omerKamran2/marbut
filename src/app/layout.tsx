253936308c795886be38c3a21fe72d2c2d4e13f8
omerKamran2 <omer.kamran100@gmail.com>
2024-11-13 02:34:45 +0500

    removed components from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 
-import Header from "@/components/header";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,10 +31,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-    
-      <Header/>
-
-
         {children}
       </body>
     </html>
