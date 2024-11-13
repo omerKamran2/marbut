8b4dc8af36d8d5da7da5e1a84bdf24e669c97455
omerKamran2 <omer.kamran100@gmail.com>
2024-11-13 20:22:23 +0500

    removed tabs from contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,28 +1,18 @@
 // newsletter.tsx
 // calendly.tsx
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
 import Newsletter from './newsletter';
 import Calendly from "./calendly";
 
 
 export default function Contact() {
     return (
-        <main>
-            <Tabs defaultValue="account" className="flex w-full">
-                <TabsList>
-                    <TabsTrigger value="newsletter">Newsletter</TabsTrigger>
-                    <TabsTrigger value="calendly">Book Demo</TabsTrigger>
-                </TabsList>
-                <TabsContent value="newsletter">
-                    <Newsletter/>
-                </TabsContent>
-                <TabsContent value="calendly">
-                    <Calendly/>
-                </TabsContent>
-            </Tabs>
+        <div className="container mx-auto p-4">
+            <div className="flex flex-col md:flex-row gap-4">
+                <Newsletter/>
+                <Calendly/>
+            </div>
 
-        </main>
+        </div>
     )
 }
