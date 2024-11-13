90e1dc4927fa22e90231d77c4e58ffcbb8b40731
omerKamran2 <omer.kamran100@gmail.com>
2024-11-13 20:23:13 +0500

    removed main wrapper from newsletter card

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -13,28 +13,26 @@ import { Button } from "./ui/button";
 
 export default function Newsletter() {
     return (
-        <main>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Newsletter Signup</CardTitle>
+        <Card className="w-full md:w-1/2 flex flex-col">
+            <CardHeader>
+                <CardTitle>Newsletter Signup</CardTitle>
                     <CardDescription>
-                    Stay updated with our latest news and offers.
+                        Stay updated with our latest news and offers.
                     </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                    <div className="space-y-1">
+            </CardHeader>
+            <CardContent className="flex-grow overflow-auto">
+                <div className="space-y-1">
                     <Label htmlFor="email">Email</Label>
                     <Input id="email" type="email" placeholder="your@email.com" />
-                    </div>
-                    <div className="space-y-1">
+                </div>
+                <div className="space-y-1">
                     <Label htmlFor="name">Name</Label>
                     <Input id="name" placeholder="Your Name" />
-                    </div>
-                </CardContent>
-                <CardFooter>
-                    <Button>Subscribe</Button>
-                </CardFooter>
-            </Card>
-        </main>
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Button>Subscribe</Button>
+            </CardFooter>
+        </Card>
     );
 }
