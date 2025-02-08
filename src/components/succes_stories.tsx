cfc5986fe9ff1d798d9cb02c44c88aee4158a4f2
omerKamran2 <omer.kamran100@gmail.com>
2025-02-08 22:59:28 +0500

    success_stories figma

--- src/components/succes_stories.tsx
+++ src/components/succes_stories.tsx
@@ -48,15 +48,24 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
+} from "@/components/ui/card"
 
 
 export default function SuccessStories() {
     return (
-            <motion.div 
+        <>
+            <div className='mx-auto my-8 max-w-7xl px-4 text-center md:my-12 lg:my-16'>
+                <h2 className="text-[48px] font-normal text-gray-900">
+                    Trusted by Founders
+                </h2>
+                <p className="mt-2 text-gray-600 text-[24px] pb-[50px] sm:pb-[100px]">
+                    We source talent that not just performs, but transforms
+                </p>
+            </div>
+            <motion.div
                 className="grid md:grid-cols-3 sm:grid-cols-1 gap-4 pb-8 card-container"
                 initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1}}
+                whileInView={{ opacity: 1 }}
                 transition={{
                     type: 'spring',
                     stiffness: 50
@@ -66,13 +75,13 @@ export default function SuccessStories() {
                     <Card key={test.id}>
                         <CardHeader>
                             <div className="flex flex-col sm:flex-row items-center sm:items-start gap-4">
-                                {/* <Image
-                                    className="rounded-full shadow-lg"
+                                <Image
+                                    className="rounded-full shadow-lg mt-6"
                                     src="/cheema.jpg"
-                                    width={100}
-                                    height={100}
+                                    width={50}
+                                    height={50}
                                     alt={`${test.name}'s profile picture`}
-                                /> */}
+                                />
                                 <div className="text-center sm:text-left pt-4">
                                     <h3 className="text-lg font-semibold">{test.name}</h3>
                                     <p className="text-sm text-muted-foreground">{test.designation}</p>
@@ -80,17 +89,17 @@ export default function SuccessStories() {
                             </div>
                         </CardHeader>
                         <CardContent>
-                        <div className="relative">
-                            <QuoteIcon className="absolute top-0 left-0 text-muted-foreground/50 h-8 w-8 -translate-x-2 -translate-y-2" />
-                            <QuoteIcon className="absolute bottom-0 right-0 text-muted-foreground/50 h-8 w-8 rotate-180" />
-                            <blockquote className="px-8 py-4 italic text-muted-foreground">
-                                {test.feedback}
-                            </blockquote>
-                        </div>
+                            <div className="relative">
+                                {/* <QuoteIcon className="absolute top-0 left-0 text-muted-foreground/50 h-8 w-8 -translate-x-2 -translate-y-2" />
+                                <QuoteIcon className="absolute bottom-0 right-0 text-muted-foreground/50 h-8 w-8 rotate-180" /> */}
+                                <blockquote className=" py-4 italic text-muted-foreground">
+                                    {test.feedback}
+                                </blockquote>
+                            </div>
                         </CardContent>
                     </Card>
                 ))}
             </motion.div>
+        </>
     );
 }
-  
