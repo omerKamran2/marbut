e2bb1989e9f5f5a04a2e037f8d1fbd982925c3e4
omerKamran2 <omer.kamran100@gmail.com>
2025-02-14 23:19:34 +0500

    remove newsletter

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -10,14 +10,13 @@ export default function Contact() {
         <div className="container mx-auto pb-8 card-container mb-[100px]">
             <div className='mx-auto my-8 max-w-7xl px-4 text-center md:my-12 lg:my-16'>
                 <h2 className="text-[48px] font-normal text-gray-900">
-                    Let's connect!
+                    Hire the Perfect Fit
                 </h2>
                 <p className="mt-2 text-gray-600 text-[24px] pb-[50px] sm:pb-[100px]">
-                    We source talent that not just performs, but transforms
+                    Book a free call with our team now!
                 </p>
             </div>
-            <div className="flex flex-col md:flex-row gap-4">
-                <Newsletter />
+            <div className="flex flex-col md:flex-row justify-center">
                 <Calendly />
             </div>
         </div>
