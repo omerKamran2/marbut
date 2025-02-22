755999bb0fddd610512b937713b67e652f2a86f0
saad <sssheikh00@gmail.com>
2025-02-23 03:43:41 +0500

    more text

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -9,10 +9,10 @@ export default function Contact() {
     return (
         <div className="container mx-auto pb-8 card-container">
             <div className='mx-auto my-8 max-w-7xl px-4 text-center md:my-12 lg:my-16'>
-                <h2 className="text-[48px] font-medium text-[#1E1E1E]">
+                <h2 className="text-[32px] md:text-[48px] font-medium text-[#1E1E1E]">
                     Hire the Perfect Fit
                 </h2>
-                <p className="pt-[32px] text-[#757575] text-[24px] font-medium">
+                <p className="pt-[32px] text-[#757575] text-[20px[ md:text-[24px] font-medium">
                     Book a free call with our team now!
                 </p>
             </div>
